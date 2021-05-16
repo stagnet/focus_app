0435559cd8bd3b69b09a3a035b271d5195820eff
Stagnet <brucewayne128gb@protonmail.com>
2021-05-16 16:50:13 +0530

    create global utilities

---
+++ src/utils/sizes.js
@@ -0,0 +1,26 @@
+export const fontSizes = {
+  sm: 14,
+  md: 18,
+  lg: 23,
+  xl: 32,
+  xxl: 40,
+  xxxl: 60,
+};
+export const paddingSizes = {
+  sm: 10,
+  md: 15,
+  lg: 24,
+  xl: 35,
+  xxl: 50,
+  xxxl: 80,
+};
+
+export const spacing = {
+  xs: 15,
+  sm: 20,
+  md: 25,
+  lg: 30,
+  xl: 35,
+  xxl: 50,
+  xxxl: 80,
+};
